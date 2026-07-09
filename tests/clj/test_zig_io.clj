ff05d09fa7d377aaa1236913dc84f5fce47ccfd0
martin roos <kulminaator@gmail.com>
2026-07-09 13:06:40 +0300

    more with-open tests

--- tests/clj/test_zig_io.clj
+++ tests/clj/test_zig_io.clj
@@ -235,10 +235,28 @@
   "copy me")
 
 ;; ============================================================
-;; with-open macro (skipped — try/finally not yet implemented)
+;; with-open macro
 ;; ============================================================
-;; (check "with-open reads content" ...)
-;; NOTE: with-open requires try/finally which is not yet implemented
+(check "with-open from user namespace"
+  (io/with-open [r (io/reader "/etc/hostname")]
+    (first (io/line-seq r)))
+  (clojure.string/trim-newline (slurp "/etc/hostname")))
+
+(check "with-open closes on normal completion"
+  (let [closed (atom false)]
+    (io/with-open [r (io/reader "/etc/hostname")]
+      (reset! closed true)
+      (first (io/line-seq r)))
+    @closed)
+  true)
+
+(check "with-open with multiple bindings"
+  (io/with-open [r1 (io/reader "/etc/hostname")
+                 r2 (io/reader "/etc/hostname")]
+    (let [l1 (first (io/line-seq r1))
+          l2 (first (io/line-seq r2))]
+      (= l1 l2)))
+  true)
 
 ;; ============================================================
 ;; Symlink operations
